d2d58031032d5bea4e34f6ed2a4533efbd654f05
aakash2028 <ajoshi3054@sdsu.edu>
2021-10-10 18:44:53 -0700

    Updating LargerOrSmaller
    Changing Print statements

--- LargerOrSmaller/script.js
+++ LargerOrSmaller/script.js
@@ -1,9 +1,9 @@
 var num1 = window.prompt("Enter the first number");
 var num2 = window.prompt("Enter the second number");
 if(num1 > num2){
-    document.write("The "+num1+" is larger than "+num2);
+    document.write(num1+" is larger than "+num2);
 }else if(num2 > num1){
-    document.write("The "+num2+" is larger than "+num1);
+    document.write(num2+" is larger than "+num1);
 }else if(num1 === num2){
     document.write("Both numbers are equal!");
 }
