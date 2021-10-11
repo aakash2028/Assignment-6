258bbffa815c5bd5bc8af7ba989228c60546b3cd
aakash2028 <ajoshi3054@sdsu.edu>
2021-10-10 19:34:38 -0700

    Changing code logic for countdown and triangle

--- Countdown/script.js
+++ Countdown/script.js
@@ -1,6 +1,5 @@
 var number = window.prompt("Enter a number");
-document.write(number,"<br>");
 while(number != 0){
-    number = number -1;
     document.write(number,"<br>");
+    number = number -1;
 }
--- LoopingATriangle/script.js
+++ LoopingATriangle/script.js
@@ -1,4 +1,8 @@
 var str = "";
-for(var i=1;i<8;i++){
-    document.write(str+= "#", "<br>");
+for(var i=0;i<8;i++){
+    for(var j=0;j<=i;j++){
+        str+="#";
+    }
+    console.log(str);
+    str = "";
 }
